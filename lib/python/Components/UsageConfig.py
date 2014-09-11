4a09052391b98353a64ae5cda1e5d9a0879796e8
rossi2000 <rossi_2000@hotmail.com>
2014-09-11 20:29:59 +0100

    [UsageConfig] change default setting

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -283,7 +283,7 @@ def InitUsageConfig():
 	config.epg.freesat = ConfigYesNo(default = True)
 	config.epg.viasat = ConfigYesNo(default = True)
 	config.epg.netmed = ConfigYesNo(default = True)
-	config.epg.virgin = ConfigYesNo(default = True)
+	config.epg.virgin = ConfigYesNo(default = False)
 
 	def EpgSettingsChanged(configElement):
 		mask = 0xffffffff
